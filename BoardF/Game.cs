ef2817f7a6631bb8403106599c0a38c36838f259
NikiNikitenko <nikinikitenkojob@gmail.com>
2022-03-06 20:40:01 +0200

    Add project files.

---
+++ BoardF/Game.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardF
+{
+    public class Game
+    {
+        int size;
+
+        public Game(int size)
+        {
+            this.size = size;
+        }
+
+        public void Start(int speep = 0)
+        { 
+        }
+
+        public int PressAt( int x, int y)
+        {
+            return 0;
+        }
+    }
+}
